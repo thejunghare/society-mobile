e12417ca52b319428005b565bef9bc16e261f6de
Roshan <roshandalvi16@gmail.com>
2024-05-18 10:48:32 +0530

    updated rn paper component use

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,79 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { Text, View, _ScrollView, Image, Button, TextInput, StyleSheet } from 'react-native';
+import { Text, View ,Image ,Button ,SafeAreaView ,TextInput} from 'react-native';
 import { useNavigation } from "@react-navigation/native";
-import { ScrollView } from "react-native-gesture-handler";
 
-const LoginScreen = () => {
+ const LoginScreen = () =>{
   const navigation = useNavigation();
+  const [text, setText] = React.useState("");
   const onPressGetStarted = () => {
-    //console.info("I was pressed");
     navigation.replace("Dashboard");
   };
 
   return (
-    // Login Screen Design Start
-    <ScrollView className={"flex-1 w-full bg-white  "}>
-      <View className="flex items-center " >
-        <Image source={require('../assets/Images/logo.png')} style={{ width: 144, height: 150, marginTop: 88 }} />
-        <Text className="font-bold text-3xl mt-2">Welcome Back!</Text>
-        <Text className=" text-lg mt-4 ">Manage your society bills, </Text>
-        <Text className="text-lg "> payments, services, complaints </Text>
-      </View>
-
-      <View className="flex items-center " >
-        <TextInput style={styles.input}
-          placeholder="Enter your email"
-        />
-        <TextInput style={styles.input}
-          placeholder="Enter password"
-        />
-        <Text style={styles.forgotPasswordText} className=" text-lg mt-14">Forgot Password</Text>
-      </View>
-
-      <View className="flex-1 items-center mt-8 mb-14 ">
-        <Button
-          onPress={onPressGetStarted}
-          title="Sign In"
-          color="#3EB489"
-          disabled={false}
-        />
-      </View>
-
-
-      <View style={styles.divider} ></View>
-      <View className="flex-1 items-center">
-        <Text >Don’t have an account? <Text style={styles.signUpText}>Sign Up</Text></Text>
-
-      </View>
-    </ScrollView>
+     // Login Screen Design Start
+     <SafeAreaView className={"flex-1 max-h-screen bg-white"}>
+           <View className="flex items-center max-h-50 " >
+                  <Image source={require('../assets/Images/logo.png')}  style={{ width: 124, height: 88, marginTop: 28 }}/>
+                  <Text className="font-bold text-3xl mt-2">Welcome Back!</Text> 
+                  <Text className=" text-lg mt-4 ">Manage your society bills, </Text>
+                  <Text className="text-lg "> payments, services, complaints </Text>  
+           </View>
+           <View className="flex items-center  max-h-50 mt-36 ">
+                 <TextInput
+                     backgroundColor="#F2F2F2"
+                     placeholder="Enter your email"
+                     mode="outlined"
+                     label="Email"
+                     value={text}
+                     onChangeText={text => setText(text)}
+                 />
+            </View>
+           <View className="flex items-center  max-h-50 mt-36 " >
+                  <Button 
+                    mode="outlined"
+                    outlineColor="true"
+                    onPress={onPressGetStarted}
+                    title="Sign in"
+                    color="#3EB489"
+                    >
+                  </Button> 
+           </View>
+</SafeAreaView>
 
   );
-
 }
-const styles = StyleSheet.create({
-  input: {
-    backgroundColor: "#F2F2F2",
-    height: 45,
-    width: 255,
-    marginTop: 30,
-    marginBottom: -15,
-    borderRadius: 22,
-    padding: 15,
-  },
-  forgotPasswordText: {
-    color: '#50C2C9',
-    fontSize: 16,
-  },
-  divider: {
-    width: '100%',
-    height: 2,
-    backgroundColor: '#F2F2F2',
-    marginVertical: 10,
-  },
-  signUpText: {
-    color: '#50C2C9',
-    fontSize: 16,
-  },
-});
 export default LoginScreen
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,35 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import * as React from "react";
-import { Text, View, Button,Image, Pressable, _ScrollView } from "react-native";
+import { View,SafeAreaView ,Image ,Text ,Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { ScrollView } from "react-native-gesture-handler";
 
 const WelcomeScreen = () => {
   const navigation = useNavigation();
   const onPressGetStarted = () => {
-    //console.info("I was pressed");
     navigation.replace("Login");
   };
   return (
     // Welcome Screen Design Start
-      <ScrollView className={"flex-1 w-full bg-white  "}>
-          <View className="flex items-center " >
-            <Image source={require('../assets/Images/logo.png')}  style={{ width: 150, height: 150, marginTop: 80 }}/>
-            <Text className="font-bold text-2xl mt-4">Gets things done with TODO</Text>
-            <Text className=" text-lg mt-4">Lorem ipsum dolor sit amet</Text>
-            <Text className="text-lg">consectetur adipiscing elit. Interdum </Text>
-            <Text className="text-lg">dictum tempus, interdum at dignissim metus.</Text>
-          </View>
+    
+     <SafeAreaView className={"flex-1 max-h-screen bg-white"}>
+                  <View className="flex items-center  max-h-50 mb-12 " >
+                          <Image source={require('../assets/Images/logo.png')} 
+                           style={{ width: 150, height: 150, marginTop: 50 }}/>
+                           <Text className="font-bold text-2xl mt-7">Gets things done with TODO</Text>
+                           <Text className=" text-lg mt-4">Lorem ipsum dolor sit amet</Text>
+                           <Text className="text-lg">consectetur adipiscing elit. Interdum </Text> 
+                           <Text className="text-lg">dictum tempus, interdum at dignissim metus.</Text> 
+                  </View> 
+                  <View className="flex items-center  max-h-50 mt-36 " >
+                               
+                           <Button 
+                           mode="outlined" 
+                           onPress={onPressGetStarted}
+                           title="Get Started"
+                           color="#3EB489"
+                           >
 
-          <View className="flex-1 items-center mt-40 ">
-            <Button
-              onPress={onPressGetStarted}
-              title="Get Started"
-              color="#3EB489"
-              disabled={false}
-              />
-          </View>
-      </ScrollView>
+                           </Button> 
+                  </View>
+      </SafeAreaView>
   );
 };
 export default WelcomeScreen;
+
