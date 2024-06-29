d2de17e59616990066edbf836103e5c3605be544
Roshan Dalvi <roshandalvi16@gmail.com>
2024-06-29 10:58:50 +0530

    ui changes

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import * as React from 'react';
-import { SafeAreaView, Text } from 'react-native';
-
-const DashboardScreen = () => {
-    return (
-        <SafeAreaView className={'flex-1 items-center justify-center'}>
-          <Text>Dashboard screen</Text>
-        </SafeAreaView>
-    );
-}
-
-export default DashboardScreen;
-=======
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { Text, View ,_ScrollView,Image,Button,TextInput} from 'react-native';
@@ -34,4 +20,3 @@ import { ScrollView } from "react-native-gesture-handler";
 }
 
 export default DashboardScreen
->>>>>>> 80e06511c88f73dbfa1d926723b99f941d286e57
